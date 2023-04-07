b7287942d19840818ad578c52f9983e83b5cbe3e
Harsith R <96562342+Coding-A-Project@users.noreply.github.com>
2023-04-07 14:15:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ while g==1:
         if move==7 and grid[2][0]=='-':grid[2][0]='X';h=1
         if move==8 and grid[2][1]=='-':grid[2][1]='X';h=1
         if move==9 and grid[2][2]=='-':grid[2][2]='X';h=1
-    while l==0:
+    while l==0 and grid[0].count("-")+grid[1].count("-")+grid[2].count("-")!=0:
         move=random.randint(1,9)
         if move==1 and grid[0][0]=="-":l=0;grid[0][0]='O';l=1
         if move==2 and grid[0][1]=="-":l=0;grid[0][1]='O';l=1
