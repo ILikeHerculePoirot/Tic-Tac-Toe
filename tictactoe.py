5e48952c64a40ee2e2da0925e216a9aece503a40
Harsith R <96562342+ILikeHerculePoirot@users.noreply.github.com>
2023-12-18 18:15:48 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -2,28 +2,11 @@ while True:
     g=[['-','-','-'],['-','-','-'],['-','-','-']]
     while True:
         m=int(input("Enter position in which you want to place X (1-9): "))
-        while m<1 or m>9:m=int(input("Enter position in which you want to place X (1-9): "))
-        if m==1 and g[0][0]=='-':g[0][0]='X'
-        if m==2 and g[0][1]=='-':g[0][1]='X'
-        if m==3 and g[0][2]=='-':g[0][2]='X'
-        if m==4 and g[1][0]=='-':g[1][0]='X'
-        if m==5 and g[1][1]=='-':g[1][1]='X'
-        if m==6 and g[1][2]=='-':g[1][2]='X'
-        if m==7 and g[2][0]=='-':g[2][0]='X'
-        if m==8 and g[2][1]=='-':g[2][1]='X'
-        if m==9 and g[2][2]=='-':g[2][2]='X'
-        if g[0].count('-')==0 and g[1].count('-')==0 and g[2].count('-')==0:
-            print(g[0][0],end=' ')
-            print(g[0][1],end=' ')
-            print(g[0][2])
-            print(g[1][0],end=' ')
-            print(g[1][1],end=' ')
-            print(g[1][2])
-            print(g[2][0],end=' ')
-            print(g[2][1],end=' ')
-            print(g[2][2])
-            print('Draw')
-            break
+        while(m<1 or m>9 or g[(m-1)//3][(m-1)%3]!='-'):m=int(input("Enter position in which you want to place X (1-9): "))
+        g[(m-1)//3][(m-1)%3]='X'
+        if((g[0][0]==g[0][1]==g[0][2] and g[0][0]=='O') or (g[1][0]==g[1][1]==g[1][2] and g[1][0]=='O') or (g[2][0]==g[2][1]==g[2][2] and g[2][0]=='O') or (g[0][0]==g[1][0]==g[2][0] and g[0][0]=='O') or (g[0][1]==g[1][1]==g[2][1] and g[0][1]=='O') or ( g[0][2]==g[1][2]==g[2][2] and g[0][2]=='O') or (g[0][0]==g[1][1]==g[2][2] and g[0][0]=='O') or (g[2][0]==g[1][1]==g[0][2] and g[2][0]=='O')):print('Loss');break
+        elif((g[0][0]==g[0][1]==g[0][2] and g[0][0]=='X') or (g[1][0]==g[1][1]==g[1][2] and g[1][0]=='X') or (g[2][0]==g[2][1]==g[2][2] and g[2][0]=='X') or (g[0][0]==g[1][0]==g[2][0] and g[0][0]=='X') or (g[0][1]==g[1][1]==g[2][1] and g[0][1]=='X') or ( g[0][2]==g[1][2]==g[2][2] and g[0][2]=='X') or (g[0][0]==g[1][1]==g[2][2] and g[0][0]=='X') or (g[2][0]==g[1][1]==g[0][2] and g[2][0]=='X')):print('Win');break
+        elif(g[0].count('-')==0 and g[1].count('-')==0 and g[2].count('-')==0):print('Draw');break
         while True:
             if g[0][0]==g[0][1] and g[0][0]=='O' and g[0][2]=='-':g[0][2]='O';break
             elif g[0][0]==g[0][2] and g[0][0]=='O' and g[0][1]=='-':g[0][1]='O';break
@@ -90,20 +73,8 @@ while True:
                     if g[1][1]=='-':g[1][1]='O';break
                     if g[1][2]=='-':g[1][2]='O';break
                     if g[2][1]=='-':g[2][1]='O';break
-        print(g[0][0],end=' ')
-        print(g[0][1],end=' ')
-        print(g[0][2])
-        print(g[1][0],end=' ')
-        print(g[1][1],end=' ')
-        print(g[1][2])
-        print(g[2][0],end=' ')
-        print(g[2][1],end=' ')
-        print(g[2][2])
-        if g[0][0]==g[0][1]==g[0][2] and g[0][0]=='O':print('Loss');break
-        elif g[1][0]==g[1][1]==g[1][2] and g[1][0]=='O':print('Loss');break
-        elif g[2][0]==g[2][1]==g[2][2] and g[2][0]=='O':print('Loss');break
-        elif g[0][0]==g[1][0]==g[2][0] and g[0][0]=='O':print('Loss');break
-        elif g[0][1]==g[1][1]==g[2][1] and g[0][1]=='O':print('Loss');break
-        elif g[0][2]==g[1][2]==g[2][2] and g[0][2]=='O':print('Loss');break
-        elif g[0][0]==g[1][1]==g[2][2] and g[0][0]=='O':print('Loss');break
-        elif g[2][0]==g[1][1]==g[0][2] and g[2][0]=='O':print('Loss');break
+        for i in range(1,10):
+            if((i-1)%3!=2):print(g[(i-1)//3][(i-1)%3],end=' ')
+            else:print(g[(i-1)//3][(i-1)%3])
+        if((g[0][0]==g[0][1]==g[0][2] and g[0][0]=='O') or (g[1][0]==g[1][1]==g[1][2] and g[1][0]=='O') or (g[2][0]==g[2][1]==g[2][2] and g[2][0]=='O') or (g[0][0]==g[1][0]==g[2][0] and g[0][0]=='O') or (g[0][1]==g[1][1]==g[2][1] and g[0][1]=='O') or ( g[0][2]==g[1][2]==g[2][2] and g[0][2]=='O') or (g[0][0]==g[1][1]==g[2][2] and g[0][0]=='O') or (g[2][0]==g[1][1]==g[0][2] and g[2][0]=='O')):print('Loss');break
+        elif((g[0][0]==g[0][1]==g[0][2] and g[0][0]=='X') or (g[1][0]==g[1][1]==g[1][2] and g[1][0]=='X') or (g[2][0]==g[2][1]==g[2][2] and g[2][0]=='X') or (g[0][0]==g[1][0]==g[2][0] and g[0][0]=='X') or (g[0][1]==g[1][1]==g[2][1] and g[0][1]=='X') or ( g[0][2]==g[1][2]==g[2][2] and g[0][2]=='X') or (g[0][0]==g[1][1]==g[2][2] and g[0][0]=='X') or (g[2][0]==g[1][1]==g[0][2] and g[2][0]=='X')):print('Win');break
